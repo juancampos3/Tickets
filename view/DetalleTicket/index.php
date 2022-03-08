7c374aa0fe1ffc0cd10cc77b5a1ba5e5a138914d
juancampos3 <jucampos3@poligran.edu.co>
2022-03-08 15:53:53 -0500

    VISUALIZAR ADJUNTOS EN DETALLE TICKET

--- view/DetalleTicket/index.php
+++ view/DetalleTicket/index.php
@@ -39,60 +39,81 @@
             </header>
 
             <div class="box-typical box-typical-padding">
-				<div class="row">
-						<div class="col-lg-6">
-							<fieldset class="form-group">
-								<label class="form-label semibold" for="cat_nom">Categoria</label>
-								<input type="text" class="form-control" name="cat_nom" id="cat_nom" readonly>
-							</fieldset>
-						</div>
-						
-                        <div class="col-lg-6">
-							<fieldset class="form-group">
-								<label class="form-label semibold" for="tick_titulo">Titulo</label>
-								<input type="text" class="form-control" name="tick_titulo" id="tick_titulo" readonly>
-							</fieldset>
-						</div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <fieldset class="form-group">
+                            <label class="form-label semibold" for="cat_nom">Categoria</label>
+                            <input type="text" class="form-control" name="cat_nom" id="cat_nom" readonly>
+                        </fieldset>
+                    </div>
 
-                        <div class="col-lg-12">
-                <fieldset class="form-group">
-                  <label class="form-label semibold" for="tickd_descripusu">Descripción</label>
-                  <div class="summernote-theme-1">
-                    <textarea id="tickd_descripusu" name="tickd_descripusu" class="summernote" name="name"></textarea>
-                  </div>
+                    <div class="col-lg-6">
+                        <fieldset class="form-group">
+                            <label class="form-label semibold" for="tick_titulo">Titulo</label>
+                            <input type="text" class="form-control" name="tick_titulo" id="tick_titulo" readonly>
+                        </fieldset>
+                    </div>
 
-                </fieldset>
-              </div>
-				</div>
+                    <div class="col-lg-12">
+                        <fieldset class="form-group">
+                            <label class="form-label semibold" for="tick_titulo">Anexos</label>
+                            <table id="documentos_data"
+                                class="table table-bordered table-striped table-vcenter js-dataTable-full">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 90%;">Nombre</th>
+                                        <th class="text-center" style="width: 10%;"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
-			</div>
+                                </tbody>
+                            </table>
+                        </fieldset>
+                    </div>
 
-            <section class="activity-line" id="lbldetalle"> 
+                    <div class="col-lg-12">
+                        <fieldset class="form-group">
+                            <label class="form-label semibold" for="tickd_descripusu">Descripción</label>
+                            <div class="summernote-theme-1">
+                                <textarea id="tickd_descripusu" name="tickd_descripusu" class="summernote"
+                                    name="name"></textarea>
+                            </div>
+
+                        </fieldset>
+                    </div>
+                </div>
+
+            </div>
+
+            <section class="activity-line" id="lbldetalle">
             </section>
 
 
             <div class="box-typical box-typical-padding" id="pnldetalle">
-				
+
                 <h6>Agregar Respuesta:</h6>
-				<div class="row">
-						<div class="col-lg-12">
-							<fieldset class="form-group">
-								<div class="summernote-theme-3">
-									<textarea id="tickd_descrip" name="tickd_descrip" class="summernote"></textarea>
-								</div>
-							</fieldset>
-						</div>
-						<div class="col-lg-12">
-							<button type="button" id="btnenviar" class="btn btn-rounded btn-inline btn-success">Enviar</button>
-                            <button type="button" id="btncerrarticket" class="btn btn-rounded btn-inline btn-danger">Cerrar Ticket</button>
-						</div>
-				</div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <fieldset class="form-group">
+                            <div class="summernote-theme-3">
+                                <textarea id="tickd_descrip" name="tickd_descrip" class="summernote"></textarea>
+                            </div>
+                        </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                        <button type="button" id="btnenviar"
+                            class="btn btn-rounded btn-inline btn-success">Enviar</button>
+                        <button type="button" id="btncerrarticket" class="btn btn-rounded btn-inline btn-danger">Cerrar
+                            Ticket</button>
+                    </div>
+                </div>
 
-			</div>
+            </div>
 
-            
+
         </div>
-    
+
     </div>
     <!--.page-content-->
     <?php require_once("../MainJs/js.php"); ?>
